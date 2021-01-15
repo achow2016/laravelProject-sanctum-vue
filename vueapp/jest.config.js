a47f948c007c596e4ee01111fd0bb5c525604c40
alan <tilldusk@gmail.com>
2021-01-14 20:25:38 -0800

    jest install, test script
    jest installed, test script index.spec.js ran without component when testing expect 1 + 1
    
    when mounting component, resulted in babel error and npm run dev not results in loop of postcss install additional dependency

---
+++ vueapp/jest.config.js
@@ -0,0 +1,14 @@
+// jest.config.js
+
+module.exports = {
+  testRegex: 'resources/js/tests/.*.spec.js$',
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
+  'transform': {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+  },
+}
---
+++ vueapp/resources/js/tests/index.spec.js
@@ -0,0 +1,21 @@
+// resources/assets/js/test/index.spec.js
+
+/*
+test('it works', () => {
+  expect(1 + 1).toBe(2)
+})
+*/
+
+import { mount } from '@vue/test-utils'
+import Login from '../components/Login.vue'
+
+test('it works', () => {
+  expect(1 + 1).toBe(2)
+})
+/*
+test('should mount without crashing', () => {
+  const wrapper = mount(Login)
+
+  expect(wrapper).toMatchSnapshot()
+})
+*/
